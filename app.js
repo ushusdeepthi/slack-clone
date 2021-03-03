f0f209cd1d431423d278816663d96478ad9228ef
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-03-03 01:11:02 +0100

    Add mongodb connection-functionality to update db

--- app.js
+++ app.js
@@ -4,13 +4,21 @@
 const express=require('express');
 const socket=require('socket.io')
 const path=require('path')
+const monk=require('monk')
+const bodyParser = require('body-parser')
 
 const app=express()
 const server=app.listen(3000)
 const io=socket(server) // opening socket connection
+const db=monk('localhost:27017/slack-clone')
 
+
+app.use((req, res, next) => {
+    req.db = db
+    next()
+})
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set('views', (__dirname, './views'));
 
@@ -19,27 +27,23 @@ app.set('views', (__dirname, './views'));
 app.get('/login',(req,resp)=>{
     resp.render('login')
 })
-app.get('/',(req,resp)=>{
+app.get('/',async(req,resp)=>{
     //get channelList from database 
 //just for test
-    let channels=[{name:'general', status:'public'}, 
-                    {name:'random', status:'public'},
-                    {name:'backend1', status:'public'},
-                    {name:'endast-elever', status:'private'}]
-                    
-    resp.render('index',{channels:channels})
+    let channel=   db.get('channels')
+    let data=await channel.find({})
+    console.log(data);
+    resp.render('index',{channels:data})
     
 })
-//for all the channels-  will render as a block to index.ejs at the end
-app.get('/channelList',(req,resp)=>{
-//get channelList from database 
-//just for test
-    // let channels=[{name:'general', status:'public'}, 
-    //                 {name:'random', status:'public'},
-    //                 {name:'backend1', status:'public'},
-    //                 {name:'endast-elever', status:'private'}]
-                    
-    // resp.render('channel.ejs',{channels:channels})
+//for individual channels  
+app.get('/:id',async(req,resp)=>{
+    let channels=   db.get('channels')
+    let channels_data=await channels.find({})
+    let channel_data=await channels.find({_id:req.params.id})
+    // console.log(channel_data);
+    resp.render('channelDetails', {channel:channel_data[0],channels:channels_data})
+
 })
 // post a new channel
 app.get('/new/channel',(req,resp)=>{
@@ -51,8 +55,28 @@ app.post('new/message',(req,resp)=>{
 })
 //log when the user connects and disconnects
 io.on('connection', socket=>{
-socket.on('chat',data=>{
+    // socket.on('joinChannel',()=>{
+        
+        
+        socket.emit('message','Welcome to Slack'); //emits only to the person connected 
+        socket.broadcast.emit('message','A user has connected')//to all otherusers than the one connected
+    // })
+ 
+//  console.log(socket.request)
+    
+socket.on('chat',(data)=>{
+
+    let channels=   db.get('channels');
+    channels.find({})
+    .then(data=>{console.log(data)})
+    channels.update({_id:data.id},{$push:{"conversation":{message:data.message, user:data.user,timeStamp:new Date()}}})
     console.log(data)  
     io.emit('chat',data)
 })
+//runs when a user disconnects
+  socket.on('disconnect',()=>{
+    io.emit('message','A user has disconnected')
+  })
+
+
 })
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,13 @@
 const socket=io('http://localhost:3000/')
+console.log(socket)
 let form=document.getElementById('form');
 let user_name=document.getElementById('name');
 let message=document.getElementById('message');
 let chat_messages= document.getElementById('chat_messages');
+let url_array=document.location.href.split('/')
+// console.log(document.location.href)
+let id=url_array[url_array.length-1];
+console.log(id)
 form.addEventListener('submit',(e)=>{
 e.preventDefault()
 if(user_name.value && message.value){
@@ -10,7 +15,9 @@ if(user_name.value && message.value){
     console.log(message.value)
 socket.emit('chat',{
     name:user_name.value,
-    message:message.value
+    message:message.value,
+    id:id
+    
 })
 }
 user_name.value='';
@@ -23,4 +30,9 @@ name_user.innerHTML=data.name
 message_user.innerHTML=data.message
 chat_messages.appendChild(name_user);
 chat_messages.appendChild(message_user);
+chat_messages.scrollTop=chat_messages.scrollHeight
+
+})
+socket.on('message',(message)=>{
+    console.log(message)
 })
